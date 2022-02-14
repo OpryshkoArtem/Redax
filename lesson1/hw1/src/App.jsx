f95f7f382468d56fa3afa23c408e27d8ac334cf4
OpryshkoArtem <opryshkoartemua@gmail.com>
2022-02-14 09:30:23 +0200

    +

--- lesson1/hw1/src/App.jsx
+++ lesson1/hw1/src/App.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import { UserDataContext } from './userdata-context.js';
+import { users } from './userdata-context.js';
 import Header from './Header.jsx';
 
 class App extends React.Component {
   state = {
-    userData: {
-      name: 'Nicola Tesla',
-      avatar_url: 'https://avatars3.githubusercontent.com/u10001',
-    },
+    userData: users.person1,
   };
 
   render() {
     return (
       <div className="page">
-        <UserDataContext.Provider value={this.state.userData}>
-          <Header />
-        </UserDataContext.Provider>
+        <Header />
       </div>
     );
   }
--- lesson1/hw1/src/userdata-context.js
+++ lesson1/hw1/src/userdata-context.js
@@ -1,3 +1,9 @@
-import React from 'react';
+import React from "react";
 
-export const UserDataContext = React.createContext({});
+export const users = {
+    person1: {
+        name: 'Nikola Tesla',
+        avatar_url: 'https://avatars3.githubusercontent.com/u10001'
+    }
+}
+export const UserDataContext = React.createContext(users.person1);
