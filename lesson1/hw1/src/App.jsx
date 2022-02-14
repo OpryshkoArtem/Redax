587230749cd1b623b8ad353058330129bcbf4ac6
OpryshkoArtem <opryshkoartemua@gmail.com>
2022-02-14 09:23:22 +0200

    provider foggot

--- lesson1/hw1/src/App.jsx
+++ lesson1/hw1/src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { users } from './userdata-context.js';
+import { UserDataContext } from './userdata-context.js';
 import Header from './Header.jsx';
 
 class App extends React.Component {
   state = {
-    userData: users.user1,
+    userData: {
+      name: 'Nicola Tesla',
+      avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    },
   };
 
   render() {
     return (
       <div className="page">
-        <Header />
+        <UserDataContext.Provider value={this.state.userData}>
+          <Header />
+        </UserDataContext.Provider>
       </div>
     );
   }
--- lesson1/hw1/src/userdata-context.js
+++ lesson1/hw1/src/userdata-context.js
@@ -1,10 +1,3 @@
 import React from 'react';
 
-export const users = {
-  user1: {
-		name: 'Nicola Tesla',
-  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
-	},
-};
-
-export const UserDataContext = React.createContext(users.user1);
+export const UserDataContext = React.createContext({});
