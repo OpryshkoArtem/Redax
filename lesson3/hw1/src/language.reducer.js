19a5178a86de678b845581b56972cd2ea7fc1b05
OpryshkoArtem <opryshkoartemua@gmail.com>
2022-02-16 10:49:36 +0200

    i

--- lesson3/hw1/src/language.reducer.js
+++ lesson3/hw1/src/language.reducer.js
@@ -1,14 +1,12 @@
 import { SET_LANGUAGE } from './language.actions';
 
-const initialState = {
-	language: 'en',
-};
+const initialState = 'en';
 
 const languageReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_LANGUAGE: {
       return {
-        language: action.payload.language,
+        state: action.payload.language,
       };
     }
     default:
