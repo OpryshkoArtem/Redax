8b5390f43cea755f54a432722ab74e59d430023e
OpryshkoArtem <opryshkoartemua@gmail.com>
2022-05-29 23:56:16 +0300

    middleWare + logger

--- lesson6/task2/src/store.js
+++ lesson6/task2/src/store.js
@@ -1,13 +1,26 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import usersReducer from './users/users.reducer';
+import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
   users: usersReducer,
 });
 
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+
+  return result;
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composeEnhancers(applyMiddleware(thunk), applyMiddleware(logger))
 );
 
 export default store;
