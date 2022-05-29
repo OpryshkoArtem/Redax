8a9e6df5cf32b3d0dc7303723a0d5391f33f9e3f
OpryshkoArtem <opryshkoartemua@gmail.com>
2022-05-30 00:01:55 +0300

    j

--- lesson6/task2/src/store.js
+++ lesson6/task2/src/store.js
@@ -1,6 +1,5 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import usersReducer from './users/users.reducer';
-import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
   users: usersReducer,
@@ -20,7 +19,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducer,
-  composeEnhancers(applyMiddleware(thunk), applyMiddleware(logger))
+  composeEnhancers(applyMiddleware(logger))
 );
 
 export default store;
